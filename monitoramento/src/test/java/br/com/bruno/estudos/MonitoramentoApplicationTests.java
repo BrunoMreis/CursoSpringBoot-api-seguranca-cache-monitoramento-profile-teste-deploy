03d327f230f95f00ca9edb34b6832d32e6c8df39
BrunoMreis <18175208+BrunoMreis@users.noreply.github.com>
2022-01-12 16:34:23 -0300

    monitoramento

---
+++ monitoramento/src/test/java/br/com/bruno/estudos/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruno.estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
